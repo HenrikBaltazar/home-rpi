8fad069dca1f2cabbd7750e854dc16a51a0164f9
rpi <henrik.g.baltazar@gmail.com>
2025-10-30 19:41:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Home Lab Raspberry Pi
+Raspberry Pi 4
+
+## Services
+- [x] Filebrowser
+- [x] PiHole
+- [x] Plex
+- [ ] Firewall
---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+# TODO
+# /dev/sda1 should be present to start
+# /dev/sda1 should be ext4, otherwise shold prompt for formatting
+# Mount -> create file structure (json file?)
+# Run docker compose
